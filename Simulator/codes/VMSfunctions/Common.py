995d11c496286f49de2993487a1c68fe2d27fd0d
VinnyDavies <vinny.davies@glasgow.ac.uk>
2018-10-30 10:35:09 +0000

    update peak class, add Sample_Datset class, edit vinny notebook

--- Simulator/codes/VMSfunctions/Common.py
+++ Simulator/codes/VMSfunctions/Common.py
@@ -20,3 +20,32 @@ class Peak(object):
         
     def __repr__(self):
         return 'Peak mz=%.4f rt=%.2f intensity=%.2f ms_level=%d' % (self.mz, self.rt, self.intensity, self.ms_level)
+    
+    def get(self,ms_level,rt,isolation_window):
+        if self.ms_level != ms_level:
+            self.get_parent_intensity = None
+            self.get_parent_mz = None
+            self.get_intensity = None
+            self.get_mz = None
+            self.in_range = None
+        elif ms_level==1: # ms1 scan of ms1 Peak
+            self.get_intensity = self.intensity
+            self.get_mz = self.mz
+            self.get_parent_intensity = None
+            self.get_parent_mz = None
+            self.in_range = None
+        else: # ms2 scan of ms2 Peak
+            self.in_range = 0
+            for i in range(0,len(isolation_window[0])):
+                if self.parent.mz > isolation_window[0][i] and self.parent.mz <= isolation_window[1][i]:
+                    self.in_range=1
+                    self.get_parent_intensity = self.parent.intensity
+                    self.get_parent_mz = self.parent.mz
+                    self.get_intensity = self.intensity
+                    self.get_mz = self.mz
+            if self.in_range == 0:
+                self.get_parent_intensity = None
+                self.get_parent_mz = None
+                self.get_intensity = None
+                self.get_mz = None
+            
--- Simulator/codes/VMSfunctions/VMS.py
+++ Simulator/codes/VMSfunctions/VMS.py
@@ -1,102 +1,30 @@
 import numpy as np
-class MS_Data(object):
-  def __init__(self,dataset,rt):
-    self.mz_ms1 = []
-    self.intensity_ms1 = []
-    self.mz_ms2 = []
-    self.intensity_ms2 = []
-    self.ms1_object = []
-    for i in range(0,len(dataset)):
-        if(dataset[i].rt==rt):
-            if(dataset[i].ms_level==1):
-                if(dataset[i] in self.ms1_object):
-                    self.mz_ms1[self.ms1_object.index(dataset[i])] = dataset[i].mz
-                    self.intensity_ms1[self.ms1_object.index(dataset[i])] = dataset[i].intensity
-                else:
-                    self.mz_ms1.append(dataset[i].mz)
-                    self.intensity_ms1.append(dataset[i].intensity)
-                    self.mz_ms2.append(None)
-                    self.intensity_ms2.append(None)
-                    self.ms1_object.append(dataset[i]) 
-            else:
-                if(dataset[i].parent in self.ms1_object):
-                    self.mz_ms2[self.ms1_object.index(dataset[i].parent)] = dataset[i].mz
-                    self.intensity_ms2[self.ms1_object.index(dataset[i].parent)] = dataset[i].intensity
-                else:
-                    self.mz_ms1.append(None)
-                    self.intensity_ms1.append(None)
-                    self.mz_ms2.append(dataset[i].mz)
-                    self.intensity_ms2.append(dataset[i].intensity)
-                    self.ms1_object.append(dataset[i].parent) 
-                    
-  def MS_Plot(self,MS_type,Index=None,width=1,xlabel=0,ylabel=0,title=0,ylim=0,xlim=0,colours=None):
-    MSplot(MS_type,self.mz_ms1,self.intensity_ms1,self.mz_ms2,self.intensity_ms2,index=Index,width=1,xlabel=0,ylabel=0,title=0,ylim=0,xlim=0,colours=None)   
+import math
+from VMSfunctions.Common import load_obj
+from VMSfunctions.DataGenerator import PeakSampler
 
-  def MS_Scan(self,range_min,range_max,plot=False,colour=False):
-    which_in_range = np.zeros(len(self.mz_ms1))
-    for i in range(0,len(range_min)):
-        which_in_range = np.add(which_in_range,np.concatenate(((np.array(self.mz_ms1)<=range_max[i]) & (np.array(self.mz_ms1)>range_min[i]))*1))
-    which_in_range2 = np.array(which_in_range)==1
-    mylist=np.array(range(0,len(self.mz_ms1)))[which_in_range2]
-    mz_ms2_list = [self.mz_ms2[i] for i in mylist]
-    intensity_ms2_list = [self.intensity_ms2[i] for i in mylist]
-    return([mz_ms2_list,intensity_ms2_list])
-    # NEED TO RETURN AN OBJECT HERE SOMEHOW???
-
-
-    #if plot==True:
-    #    intensity_ms2_list = [x for _,x in sorted(zip(mz_ms2_list,intensity_ms2_list))]
-    #    mz_ms2_list.sort()
-    #    if colour==True:
-    #        available_colours=["b","g","r","c","m","y","k"]
-    #        colours=[]
-    #        for i in range(0,len(mz_ms2_list)):
-    #            colours.append([available_colours[i%7]]*len(mz_ms2_list[i]))
-    #        colours = sum(colours,[])
-    #        MSplot(MStype="MS2",Data_mz_MS2=[sum(mz_ms2_list,[])],Data_intensity_ms2=[sum(intensity_ms2_list,[])],index=0,colours=colours)
-    #    else:
-    #        MSplot(MStype="MS2",Data_mz_MS2=[sum(mz_ms2_list,[])],Data_intensity_ms2=[sum(intensity_ms2_list,[])],index=0)
-
-  def MS_MultiScan(self,scan_bottom_initial,scan_top_initial,scan_bottom_secondary,scan_top_secondary):
-    scans = []
-    scan_location = []
-    scan_result = [[] for e in range(0,len(scan_bottom_initial))] 
-    for i in range(0,len(scan_bottom_initial)):
-        which_in_range = np.zeros(len(self.mz_ms1))
-        for j in range(0,len(scan_bottom_initial[i])):
-            which_in_range = np.add(which_in_range,np.concatenate(((np.array(self.mz_ms1)<=scan_top_initial[i][j]) & (np.array(self.mz_ms1)>scan_bottom_initial[i][j]))*1))
-        which_in_range2 = np.array(which_in_range)==1
-        mylist=np.array(range(0,len(self.mz_ms1)))[which_in_range2]
-        mz_ms2_list = [self.mz_ms2[i] for i in mylist]
-        scans.extend(sum(mz_ms2_list,[]))
-        scan_location.append(sum([scan_bottom_initial[i],scan_top_initial[i]],[]))
-        for j in range(0,len(scan_bottom_initial)):
-            if j==i:
-                scan_result[j].extend([1]*len(sum(mz_ms2_list,[])))
-            else:
-                scan_result[j].extend([0]*len(sum(mz_ms2_list,[])))
-    if len(set(scans))!=len(scans):
-        sys.exit("An area has been scanned twice in the initial set up phase")
-
-    for i in range(0,len(scan_bottom_secondary)):
-        which_in_range = np.zeros(len(self.mz_ms1))
-        for j in range(0,len(scan_bottom_secondary[i])):
-            which_in_range = np.add(which_in_range,np.concatenate(((np.array(self.mz_ms1)<=scan_top_secondary[i][j]) & (np.array(self.mz_ms1)>scan_bottom_secondary[i][j]))*1))
-        which_in_range2 = np.array(which_in_range)==1
-        mylist=np.array(range(0,len(self.mz_ms1)))[which_in_range2]
-        mz_ms2_list = [self.mz_ms2[i] for i in mylist]
-        if len(scan_bottom_secondary[i])==1:
-            scan_location.append(sum([scan_bottom_secondary[i],scan_top_secondary[i]],[]))
-        else:
-            scan_location.append([scan_bottom_secondary[i],scan_top_secondary[i]])
-        ms2_s = sum(mz_ms2_list,[])
-        s_scan = [None]*len(scans)
-        for j in range(0,len(scans)):
-            if scans[j] in sum(mz_ms2_list,[]):
-                s_scan[j]=1
-            else:
-                s_scan[j]=0
-        scan_result.extend([s_scan])
-    scans=[scans]
-    scans.extend(scan_result)
-    return({'Scan Results':scans, 'Scan Locations':scan_location})
+class Sample_Dataset:
+    def __init__ (self,file_location,n):
+        ps = load_obj(file_location)
+        ms1 = ps.sample(ms_level=1, n_peaks=n)
+        ms2 = ps.sample(ms_level=2, n_peaks=n)
+        for i in range(0,n):
+            ms2[i].parent = ms1[i]
+            ms1[i].rt = 0
+            ms2[i].rt = 0
+        self.peaks = ms1 + ms2
+        self.ms1_names = []
+        self.ms2_names = []
+        self.ms1_range = [math.inf,-math.inf]
+        for dataset_index in range(0,len(self.peaks)):
+            if self.peaks[dataset_index].ms_level == 2:
+                self.ms1_names.append(self.peaks[dataset_index].parent)
+                self.ms2_names.append(self.peaks[dataset_index])
+                self.ms1_range = np.array([min(self.ms1_range[0],self.peaks[dataset_index].parent.mz), max(self.ms1_range[1],self.peaks[dataset_index].parent.mz)]).tolist()
+                
+    def ms1_values(self):
+        self.ms1_values = []
+        for dataset_index in range(0,len(self.peaks)):
+            if self.peaks[dataset_index].ms_level == 1:
+                self.ms1_values.append(self.peaks[dataset_index].mz)
+        self.ms1_values = sum(np.array(self.ms1_values).tolist(),[])
