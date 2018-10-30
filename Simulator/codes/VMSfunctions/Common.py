280df008cd03ab9ec39ea33068313b7f9b6a0b63
Simon Rogers <simon.d.rogers@gmail.com>
2018-10-30 11:11:55 +0000

    some changes to Common from Simon

--- Simulator/codes/VMSfunctions/Common.py
+++ Simulator/codes/VMSfunctions/Common.py
@@ -21,31 +21,70 @@ class Peak(object):
     def __repr__(self):
         return 'Peak mz=%.4f rt=%.2f intensity=%.2f ms_level=%d' % (self.mz, self.rt, self.intensity, self.ms_level)
     
-    def get(self,ms_level,rt,isolation_window):
-        if self.ms_level != ms_level:
-            self.get_parent_intensity = None
-            self.get_parent_mz = None
-            self.get_intensity = None
-            self.get_mz = None
-            self.in_range = None
-        elif ms_level==1: # ms1 scan of ms1 Peak
-            self.get_intensity = self.intensity
-            self.get_mz = self.mz
-            self.get_parent_intensity = None
-            self.get_parent_mz = None
-            self.in_range = None
-        else: # ms2 scan of ms2 Peak
-            self.in_range = 0
-            for i in range(0,len(isolation_window[0])):
-                if self.parent.mz > isolation_window[0][i] and self.parent.mz <= isolation_window[1][i]:
-                    self.in_range=1
-                    self.get_parent_intensity = self.parent.intensity
-                    self.get_parent_mz = self.parent.mz
-                    self.get_intensity = self.intensity
-                    self.get_mz = self.mz
-            if self.in_range == 0:
-                self.get_parent_intensity = None
-                self.get_parent_mz = None
-                self.get_intensity = None
-                self.get_mz = None
-            
+    
+    def get(self,ms_level,rt,isolation_windows):
+        if not ms_level == self.ms_level:
+            return None
+        if not self._rt_match(rt):
+            return None
+        if ms_level == 1:
+            if self._isolation_match(isolation_windows):
+                return (self._get_mz(rt),self._get_intensity())
+            else:
+                return None
+        # if we get here, it's ms_level >1 and rt is ok
+        if(parent._isolation_match(isolation_windows)):
+            return (self._get_mz(rt),self._get_intensity())
+
+    def _get_mz(self,rt):
+        return self.mz
+
+    def _get_intensity(self,mz):
+        return self.intensity
+
+    # def get(self,ms_level,rt,isolation_window):
+    #     if self.ms_level != ms_level:
+    #         self.get_parent_intensity = None
+    #         self.get_parent_mz = None
+    #         self.get_intensity = None
+    #         self.get_mz = None
+    #         self.in_range = None
+    #     elif ms_level==1: # ms1 scan of ms1 Peak
+    #         self.get_intensity = self.intensity
+    #         self.get_mz = self.mz
+    #         self.get_parent_intensity = None
+    #         self.get_parent_mz = None
+    #         self.in_range = None
+    #     else: # ms2 scan of ms2 Peak
+    #         self.in_range = 0
+    #         for i in range(0,len(isolation_window[0])):
+    #             if self.parent.mz > isolation_window[0][i] and self.parent.mz <= isolation_window[1][i]:
+    #                 self.in_range=1
+    #                 self.get_parent_intensity = self.parent.intensity
+    #                 self.get_parent_mz = self.parent.mz
+    #                 self.get_intensity = self.intensity
+    #                 self.get_mz = self.mz
+    #         if self.in_range == 0:
+    #             self.get_parent_intensity = None
+    #             self.get_parent_mz = None
+    #             self.get_intensity = None
+    #             self.get_mz = None
+
+    def _rt_match(self,rt):
+        if not rt:
+            return True
+        if self.ms_level == 1:
+            return True # eventually, some checking here and possibly returning false
+        else:
+            return self.parent._rt_match(rt)
+    def _isolation_match(self,isolation_windows):
+        # assumes list is formated like:
+        # [(min_1,max_1),(min_2,max_2),...],
+        if not isolation_windows:
+            return True
+        for window in isolation_windows:
+            if(self.mz > window[0] and self.mz < window[1]):
+                return True
+        return False
+        # for i in range(0,len(isolation_window[0])):
+        #         if self.parent.mz > isolation_window[0][i] and self.parent.mz <= isolation_window[1][i]:
