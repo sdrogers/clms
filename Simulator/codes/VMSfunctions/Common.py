113219ad1494aa20cdd3c70a6cbfd07edf63c58d
Joe <joewandy@gmail.com>
2018-11-14 09:36:25 +0000

    Make loading faster

--- Simulator/codes/VMSfunctions/Common.py
+++ Simulator/codes/VMSfunctions/Common.py
@@ -1,12 +1,45 @@
 import pickle
+from sklearn.externals import joblib
 
-def save_obj(obj, filename):
+# some useful constants
+MZ = 'mz'
+INTENSITY = 'intensity'
+MZ_INTENSITY = MZ + '_' + INTENSITY
+RT = 'rt'
+N_PEAKS = 'n_peaks'
+
+
+def save_obj(obj, filename, use_joblib=True):
+    """
+    Save object to file
+    :param obj: the object to save
+    :param filename: the output file
+    :param sklearn: if true, use joblib to dump the model
+    :return: None
+    """
     with open(filename, 'wb') as f:
-        pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)    
+        if use_joblib:
+            # Supposedly using joblib works better for numpy array, see:
+            # - https://scikit-learn.org/stable/modules/model_persistence.html
+            # - http://gael-varoquaux.info/programming/new_low-overhead_persistence_in_joblib_for_big_data.html
+            joblib.dump(obj, f, compress=3)
+        else:
+            pickle.dump(obj, f)
 
-def load_obj(filename):
-    with open(filename, 'rb') as f:        
-        return pickle.load(f)            
+
+def load_obj(filename, use_joblib=True):
+    """
+    Load saved object from file
+    :param filename: The file to load
+    :param joblib: If true, use joblib to load the model
+    :return: the loaded object
+    """
+    with open(filename, 'rb') as f:
+        if use_joblib:
+            return joblib.load(f)
+        else:
+            return pickle.load(f)
+
 
 class Peak(object):
     def __init__(self, mz, rt, intensity, ms_level, parent=None, filename=None, scan_number=None):
@@ -17,45 +50,46 @@ class Peak(object):
         self.filename = filename
         self.scan_number = scan_number
         self.parent = parent
-        
+
     def __repr__(self):
         return 'Peak mz=%.4f rt=%.2f intensity=%.2f ms_level=%d' % (self.mz, self.rt, self.intensity, self.ms_level)
-    
-    
-    def get(self,ms_level,rt,isolation_windows):
+
+    def get(self, ms_level, rt, isolation_windows):
         if not ms_level == self.ms_level:
             return None
         if not self._rt_match(rt):
             return None
         if ms_level == 1:
             if self._isolation_match(isolation_windows):
-                return (self._get_mz(rt),self._get_intensity())
+                return (self._get_mz(rt), self._get_intensity())
             else:
                 return None
-        # if we get here, it's ms_level >1 and rt is ok
-        if(self.parent._isolation_match(isolation_windows)):
-            return (self._get_mz(rt),self._get_intensity(rt))
 
-    def _get_mz(self,rt):
+        # if we get here, it's ms_level >1 and rt is ok
+        if (self.parent._isolation_match(isolation_windows)):
+            return (self._get_mz(rt), self._get_intensity(rt))
+
+    def _get_mz(self, rt):
         return self.mz
 
-    def _get_intensity(self,rt):
+    def _get_intensity(self, rt):
         return self.intensity
 
-    def _rt_match(self,rt):
+    def _rt_match(self, rt):
         if not rt:
             return True
         if self.ms_level == 1:
-            return True # eventually, some checking here and possibly returning false
+            return True  # eventually, some checking here and possibly returning false
         else:
             return self.parent._rt_match(rt)
-    def _isolation_match(self,isolation_windows):
+
+    def _isolation_match(self, isolation_windows):
         # assumes list is formated like:
         # [(min_1,max_1),(min_2,max_2),...],
         if not isolation_windows:
             return True
         for window in isolation_windows:
-            if(self.mz > window[0] and self.mz < window[1]):
+            if (self.mz > window[0] and self.mz < window[1]):
                 return True
         return False
         # for i in range(0,len(isolation_window[0])):
