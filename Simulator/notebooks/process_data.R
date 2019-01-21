1dde2000d4a5b95490cbdae9eadd4618c924f075
Joe <joewandy@gmail.com>
2019-01-21 10:49:55 +0000

    Updated R script

--- Simulator/notebooks/process_data.R
+++ Simulator/notebooks/process_data.R
@@ -10,9 +10,9 @@ getDesign = function(mzml_files, label) {
   return(design)  
 }
 
-readData = function(files, design, mode="onDisk", msLevel=1) {
+readData = function(files, design, mode="onDisk") {
   raw_data <- MSnbase::readMSData(files = files, pdata = new("NAnnotatedDataFrame", design),
-                                  mode = mode, msLevel. = msLevel, centroided. = TRUE)
+                                  mode = mode, centroided. = TRUE, msLevel. = 1)
   return(raw_data)
 }
 
@@ -33,41 +33,44 @@ filterDf <- function(df, rt_start, rt_end, min_intensity, min_sn) {
   return(peak_info)  
 }
 
-get_feature = function(raw_data, row, msLevel) {
+get_feature = function(data, row, mzppm=NULL, make_plot=FALSE) {
   sample <- row$sample
   mz_range_df <- row[, c('mzmin', 'mzmax')]
   rt_range_df <- row[, c('rtmin', 'rtmax')]
 
   # TODO: we need to add some padding to the mz_range to get the complete chromatogram, why?!
-  mz_range <- c(mz_range_df$mzmin-0.0003, mz_range_df$mzmax+0.0003)
-  
-  # alternatively we have a ppm constant for the bounds
-  # mz_range = c(mz * (1-mzppm/1e6), mz * (1+mzppm/1e6))
-  
+  # alternatively we can also have a ppm constant for the bounds
+  mz = row$mz
+  if (is.null(mzppm)) {
+    mz_range <- c(mz_range_df$mzmin-0.0003, mz_range_df$mzmax+0.0003)
+  } else {
+    mz_range <- c(mz * (1-mzppm/1e6), mz * (1+mzppm/1e6))
+  }
   rt_range <- c(rt_range_df$rtmin, rt_range_df$rtmax)
 
   # below will not give you the mz values of each data point  
-  # chr_raw <- chromatogram(raw_data, mz=mz_range, rt=rt_range)
+  # chr_raw <- chromatogram(data, mz=mz_range, rt=rt_range)
   # chr_raw <- chr_raw[1, sample]
 
   # below is the correct code
-  one_file = filterFile(raw_data, sample)
+  one_file = filterFile(data, sample)
+  sample_name = one_file@phenoData@data$sample_name
   filtered_file = filterRt(filterMz(one_file, mz_range), rt_range)
   chrom_data = as(filtered_file, 'data.frame')
-  # plotMsData(chrom_data)
+  
+  if (make_plot) {
+    plotMsData(chrom_data)
+  }
 
-  # TODO: normalise the rt values, first value (start) is 0
-  # TODO: normalise the mz values, the average is 0
-  # TODO: not sure about the intensities
   mz_values = chrom_data$mz
   rt_values = chrom_data$rt
   intensity_values = chrom_data$i
   
-  return(list(id=rownames(row), mz=row$mz, mzmin=row$mzmin, mzmax=row$mzmax, 
+  return(list(id=rownames(row), mz=mz, mzmin=row$mzmin, mzmax=row$mzmax, 
               rt=row$rt, rtmin=row$rtmin, rtmax=row$rtmax,
               into=row$into, maxo=row$maxo, sn=row$sn, 
-              mz_values = mz_values, rt_values = rt_values, intensity_values = intensity_values,
-              msLevel=msLevel, fromFile=sample))    
+              mz_values=mz_values, rt_values=rt_values, intensity_values=intensity_values,
+              msLevel=1, fromFile=sample, sample_name=sample_name))    
 }
 
 sortDf <- function(df, decreasing=TRUE) {
@@ -75,16 +78,16 @@ sortDf <- function(df, decreasing=TRUE) {
   return(speaks)
 }
 
-get_all_features <- function(data, filtered_list, msLevel, N) {
+get_all_features <- function(data, filtered_list, N, mzppm=NULL, make_plot=FALSE) {
   start.time <- Sys.time()
   # all_features <- lapply(filtered_list[1:N], function(row) {
-  #   feature <- get_feature(data, row, msLevel)
+  #   feature <- get_feature(data, row)
   # })
   all_features <- vector("list", length = N)
   for (i in 1:N) {
     print(paste(i, '/', N))
     row <- filtered_list[[i]]
-    all_features[[i]] <- get_feature(data, row, msLevel)
+    all_features[[i]] <- get_feature(data, row, mzppm=mzppm, make_plot=make_plot)
   }
   end.time <- Sys.time()
   time.taken <- end.time - start.time
@@ -92,7 +95,7 @@ get_all_features <- function(data, filtered_list, msLevel, N) {
   return(all_features)  
 }
 
-get_all_features_par <- function(data, filtered_list, msLevel, N) {
+get_all_features_par <- function(data, filtered_list, N, mzppm=NULL, make_plot=FALSE) {
   library(foreach)
   library(doParallel)
   no_cores <- detectCores() - 1
@@ -101,10 +104,10 @@ get_all_features_par <- function(data, filtered_list, msLevel, N) {
   start.time <- Sys.time()
 
   # use biocparallel instead?
-  # all_features = bplapply(1:N, function(i, filtered_list, msLevel, get_feature) {
+  # all_features = bplapply(1:N, function(i, filtered_list, get_feature) {
   #   row <- filtered_list[[i]]
-  #   feature <- get_feature(data, row, msLevel)
-  # }, filtered_list, msLevel, get_feature)
+  #   feature <- get_feature(data, row)
+  # }, filtered_list, get_feature)
   
   all_features <- foreach(i=1:N,
                           .combine = list,
@@ -112,7 +115,7 @@ get_all_features_par <- function(data, filtered_list, msLevel, N) {
                           .export = 'get_feature',
                           .packages = 'xcms') %dopar% {
                             row <- filtered_list[[i]]
-                            feature <- get_feature(data, row, msLevel)
+                            feature <- get_feature(data, row, mzppm=mzppm, make_plot=make_plot)
                           }
   end.time <- Sys.time()
   time.taken <- end.time - start.time
@@ -120,38 +123,57 @@ get_all_features_par <- function(data, filtered_list, msLevel, N) {
   return(all_features)
 }
 
+extract_peaks <- function(mzml_files, design, ppm, peakwidth, snthresh, prefilter, mzdiff) {
+  # TODO: at the moment, findChromPeaks only works with OnDiskMSnExp, that's why we pass it raw_data_on_disk for peak picking
+  raw_data <- readData(mzml_files, design, mode='onDisk')
+  xdata <- pickPeaks(raw_data, ppm=ppm, peakwidth=peakwidth, snthresh=snthresh, prefilter=prefilter, mzdiff=mzdiff)
+  return(xdata)
+}
+
+extract_features <- function(data, mzppm, make_plot, N=NULL) {
+  peaks_df <- data.frame(chromPeaks(data))
+  sorted_df <- sortDf(peaks_df, TRUE)
+  filtered_list <-  setNames(split(sorted_df, seq(nrow(sorted_df))), rownames(sorted_df)) # convert df to list
+  
+  if (is.null(N)) {
+    N <- length(filtered_list)
+  }
+  # all_features_par <- get_all_features_par(data, filtered_list, N, mzppm=mzppm)
+  all_features <- get_all_features(data, filtered_list, N, mzppm=mzppm, make_plot=make_plot)
+  
+  # remove features that have no chromatographic peak shapes
+  selected <- all_features[sapply(all_features, function(f) length(f$mz_values)>0)]
+  return(selected)
+}
+
+get_df = function(all_features) {
+  dfs <- lapply(all_features, data.frame, stringsAsFactors = FALSE) # list of dataframes, one for each peak
+  df <- do.call("rbind", dfs) # a single big dataframe
+  return(df)  
+}
+
+write_df <- function(df, filename) {
+  write.csv(df, file = gzfile(filename), row.names = FALSE)  
+}
+
 ### processing starts here ###
 
 mzml_dir <- 'C:\\Users\\joewa\\Work\\docs\\clms\\FourBeers_mzML_files\\POS\\'
 mzml_files <- dir(mzml_dir, full.names=TRUE)
 design <- getDesign(mzml_files, "BEER")
-msLevel = 1
-raw_data_on_disk <- readData(mzml_files, design, mode='onDisk', msLevel=msLevel)
-raw_data_in_memory <- readData(mzml_files, design, mode='inMemory', msLevel=msLevel)
 
 # ppm value is set fairly large.
 # Other parameters for peakwidth, snthresh, prefilter for justin's data, taken from 
 # https://www.dropbox.com/home/Meta_clustering/ms2lda/large_study/r/beer_method_3_pos?preview=xcmsPeakPicking.R
-# TODO: at the moment, findChromPeaks only works with OnDiskMSnExp, that's why we pass it raw_data_on_disk for peak picking
-xdata <- pickPeaks(raw_data_on_disk, ppm=10, peakwidth=c(5, 100), snthresh=3, prefilter=c(3, 1000), mzdiff=0.001)
-peaks_df <- data.frame(chromPeaks(xdata))
+ppm = 10
+peakwidth = c(5, 100)
+snthresh = 3
+prefilter = c(3, 1000)
+mzdiff = 0.001
+mzppm <- 10 # the ppm value used to draw the bounding box to retrieve chromatographic peak
+make_plot = FALSE
 
-# Filtering parameters, taken from 
-# see https://www.dropbox.com/home/Meta_clustering/ms2lda/large_study/r/config?preview=config_beer_pos_3.yml
-#     rt_start: 3                         # too early (top of peak)
-#     rt_end: 21                          # too late (top of peak)
-#     min_MS1_intensity : 1E5 or 2E5 should do a decent job on Polyomics Q-Exactive data
-#     min_MS2_intensity: 5000
-# filtered_df <- filterDf(peaks_df, rt_start=3*60, rt_end=21*60, min_intensity=1E6, min_sn=100)
-# sorted_df <- sortDf(filtered_df, TRUE)
-
-# sort dataframe by integrated intensity descending
-sorted_df <- sortDf(peaks_df, FALSE)
-filtered_list <-  setNames(split(sorted_df, seq(nrow(sorted_df))), rownames(sorted_df)) # convert df to list
-
-N <- length(filtered_list)
-# all_features_par <- get_all_features_par(xdata, filtered_list, msLevel, N)
-all_features <- get_all_features(xdata, filtered_list, msLevel, N)
-dfs <- lapply(all_features, data.frame, stringsAsFactors = FALSE) # list of dataframes, one for each peak
-df <- do.call("rbind", dfs) # a single big dataframe
-write.csv(df, file = 'peaks.csv', row.names = FALSE)
+data <- extract_peaks(mzml_files, design, ppm, peakwidth, snthresh, prefilter, mzdiff)
+ms1_features <- extract_features(data, mzppm, make_plot)
+df = get_df(ms1_features)
+write_df(df, 'beer_ms1_peaks.csv.gz')
