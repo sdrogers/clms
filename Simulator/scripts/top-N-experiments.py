ab9cce1753d8ff645d0afe14153ddaa9145889f0
Joe <joewandy@gmail.com>
2019-04-03 16:29:51 +0100

    Notebook update

--- Simulator/scripts/top-N-experiments.py
+++ Simulator/scripts/top-N-experiments.py
@@ -5,6 +5,7 @@ sys.path.append('../codes')
 from VMSfunctions.Controller import *
 from VMSfunctions.Common import *
 
+set_log_level_debug()
 ps = load_obj('../models/peak_sampler_4_beers.p')
 xcms_output = '../models/beer_ms1_peaks.csv.gz'
 cc = ChromatogramCreator(xcms_output)
@@ -15,13 +16,7 @@ mz_tol = 5  # the mz isolation window around a selected precursor ion
 rt_tol = 15  # the rt window around a selected precursor ion to prevent it from fragmented multiple times
 min_ms2_intensity = 5000  # the minimum ms2 peak intensity
 
+set_log_level_warning()
 mass_spec = IndependentMassSpectrometer(POSITIVE, cc.chemicals, density=ps.density_estimator)
 controller = TopNController(mass_spec, N, mz_tol, rt_tol, min_ms2_intensity=min_ms2_intensity)
-
-set_log_level_info()
-controller.make_plot = False
-
-# set_log_level_debug()
-# controller.make_plot = True
-
-controller.run(max_rt)
+controller.run(0, max_rt)
