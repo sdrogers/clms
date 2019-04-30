c755058d8b78124bae3994cbd257b551624871fe
Joe Wandy <joewandy@gmail.com>
2019-04-30 11:58:33 +0100

    Update debugging script.

--- Simulator/scripts/top-N-experiments.py
+++ Simulator/scripts/top-N-experiments.py
@@ -2,21 +2,33 @@ import sys
 
 sys.path.append('../codes')
 
+from VMSfunctions.Chemicals import *
+from VMSfunctions.Chromatograms import *
+from VMSfunctions.MassSpec import *
 from VMSfunctions.Controller import *
 from VMSfunctions.Common import *
+from VMSfunctions.DataGenerator import *
 
 set_log_level_debug()
-ps = load_obj('../models/peak_sampler_4_beers.p')
-xcms_output = '../models/beer_ms1_peaks.csv.gz'
-cc = ChromatogramCreator(xcms_output)
 
-max_rt = 10  # the maximum retention time of scans to generate
-N = 5  # top-5 DDA fragmentation
-mz_tol = 5  # the mz isolation window around a selected precursor ion
-rt_tol = 15  # the rt window around a selected precursor ion to prevent it from fragmented multiple times
-min_ms2_intensity = 5000  # the minimum ms2 peak intensity
+base_dir = 'C:\\Users\\joewa\\University of Glasgow\\Vinny Davies - CLDS Metabolomics Project\\'
+ps = load_obj(base_dir + 'Trained Models\\peak_sampler_beer1pos_fragmentation.p')
 
-set_log_level_warning()
-mass_spec = IndependentMassSpectrometer(POSITIVE, cc.chemicals, density=ps.density_estimator)
-controller = TopNController(mass_spec, N, mz_tol, rt_tol, min_ms2_intensity=min_ms2_intensity)
-controller.run(0, max_rt)
+base_dir = 'C:\\Users\\joewa\\Work\\git\\clms\\Simulator'
+dataset = load_obj(os.path.join(base_dir, 'models\\dda_results\\dataset.p'))
+noisy_dataset = load_obj(os.path.join(base_dir, 'models\\dda_results\\noisy_dataset.p'))
+
+isolation_window = 0.5   # the isolation window in Dalton around a selected precursor ion
+ionisation_mode = POSITIVE
+data = noisy_dataset
+density = ps.density_estimator
+min_ms1_intensity = 2.5E5 # minimum ms1 intensity to fragment
+min_rt = 3*60
+max_rt = 21*60
+N = 10
+rt_tol = 15
+
+mass_spec = IndependentMassSpectrometer(ionisation_mode, data, density=density)
+controller = TopNController(mass_spec, N, isolation_window,
+                            rt_tol, min_ms1_intensity)
+controller.run(min_rt, max_rt, True)
