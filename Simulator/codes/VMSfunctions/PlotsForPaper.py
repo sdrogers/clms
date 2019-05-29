0dad7463caf9c35caed5f444d446d5be6f6b96ad
Joe Wandy <joewandy@gmail.com>
2019-05-29 23:59:05 +0100

    Added analysis functions for the manuscript.

--- Simulator/codes/VMSfunctions/PlotsForPaper.py
+++ Simulator/codes/VMSfunctions/PlotsForPaper.py
@@ -1,11 +1,3 @@
-import os
-import pathlib
-import numpy as np
-import sys
-import scipy.stats
-import re
-import os
-import pandas as pd
 import seaborn as sns
 
 from VMSfunctions.Controller import *
@@ -16,6 +8,14 @@ def get_N(row):
         return 10
     else:
         return row['filename'].split('_')[3]
+
+
+def get_dew(row):
+    if 'T10' in row['filename']:
+        return 15
+    else:
+        tok = row['filename'].split('_')[5] # get the dew value in the filename
+        return tok.split('.')[0] # get the part before '.mzML'
 
 
 def experiment_group(row):
@@ -48,6 +48,9 @@ def get_df(csv_file, min_ms1_intensity, rt_range, mz_range):
         df[['N']] = df[['N']].astype('int')
     except IndexError:
         pass
+    except ValueError:
+        df['N'] = df.apply(lambda row: np.nan, axis=1)
+
     # add group column
     df['group'] = df.apply(lambda row: experiment_group(row), axis=1)
     return df
@@ -106,9 +109,9 @@ def find_chem(to_find, min_rts, max_rts, min_mzs, max_mzs, chem_list):
         return None
     elif len(matches) == 1:
         return matches[0]
-    else: # multiple matches, take the closest in intensity
-        intensity_diffs = [np.abs(chem.max_intensity - to_find.max_intensity) for chem in matches]
-        idx = np.argmin(intensity_diffs)
+    else:  # multiple matches, take the closest in rt
+        diffs = [np.abs(chem.rt - to_find.rt) for chem in matches]
+        idx = np.argmin(diffs)
         return matches[idx]
 
 
@@ -117,8 +120,8 @@ def match(chemical_list_1, chemical_list_2, mz_tol, rt_tol, verbose=False):
     chem_list = np.array(chemical_list_2)
     min_rts = np.array([chem.rt - rt_tol for chem in chem_list])
     max_rts = np.array([chem.rt + rt_tol for chem in chem_list])
-    min_mzs = np.array([chem.isotopes[0][0] * (1-mz_tol/1e6) for chem in chem_list])
-    max_mzs = np.array([chem.isotopes[0][0] * (1+mz_tol/1e6) for chem in chem_list])
+    min_mzs = np.array([chem.isotopes[0][0] * (1 - mz_tol / 1e6) for chem in chem_list])
+    max_mzs = np.array([chem.isotopes[0][0] * (1 + mz_tol / 1e6) for chem in chem_list])
     for i in range(len(chemical_list_1)):
         to_find = chemical_list_1[i]
         if i % 1000 == 0 and verbose:
@@ -127,3 +130,122 @@ def match(chemical_list_1, chemical_list_2, mz_tol, rt_tol, verbose=False):
         if match:
             matches[to_find] = match
     return matches
+
+
+def check_found_matches(matches, left_label, right_label, N=20):
+    found = [key for key in matches if matches[key] is not None]
+    print('Found %d/%d (%f)' % (len(found), len(matches), len(found) / len(matches)))
+
+    print('%s\t\t\t\t\t\t%s' % (left_label, right_label))
+    for key, value in list(matches.items())[0:N]:
+        if value is not None:
+            print('mz %.2f rt %.4f intensity %.4f\tmz %.2f rt %.4f intensity %.4f' % (
+                key[0], key[1], key[2], value[0], value[1], value[2]))
+
+
+def load_controller(results_dir, experiment_name, N, rt_tol):
+    analysis_name = 'experiment_%s_N_%d_rttol_%d' % (experiment_name, N, rt_tol)
+    pickle_in = '%s/%s.p' % (results_dir, analysis_name)
+    print('Loading %s' % analysis_name)
+    try:
+        controller = load_obj(pickle_in)
+    except FileNotFoundError:
+        controller = None
+    return controller
+
+
+def load_controllers(results_dir, Ns, rt_tols):
+    controllers = []
+    for N in Ns:
+        for rt_tol in rt_tols:
+            controller = load_controller(results_dir, N, rt_tol)
+            if controller is not None:
+                controllers.append(controller)
+    return controllers
+
+
+def compute_performance(controller, dataset, min_ms1_intensity):
+    ms_level = 2
+    chem_to_frag_events = get_frag_events(controller, ms_level)
+
+    # positive instances are ground truth MS1 peaks found by XCMS
+    # negative instances are chemicals that cannot be matched to XCMS output
+    positives = list(filter(lambda x: x.found_in_ms1, dataset))
+    negatives = list(filter(lambda x: not x.found_in_ms1, dataset))
+
+    # for both positive and negative instances, count how many frag events they have
+    # and whether it's above (good) or below (bad) the minimum ms1 intensity at the time of fragmentation.
+    positives_count = get_chem_frag_counts(positives, chem_to_frag_events, min_ms1_intensity)
+    negatives_count = get_chem_frag_counts(negatives, chem_to_frag_events, min_ms1_intensity)
+
+    # TP = positive instances that are good only
+    tp = [chem for chem in positives if positives_count[chem]['good'] > 0 and positives_count[chem]['bad'] == 0]
+
+    # FP = negative instances that are fragmented (both good + bad)
+    fp = [chem for chem in negatives if negatives_count[chem]['good'] > 0 or negatives_count[chem]['bad'] > 0]
+
+    # FN = positive instances that are not fragmented at all + positive instances that are bad only
+    fn = [chem for chem in positives if \
+            (positives_count[chem]['good'] == 0 and positives_count[chem]['bad'] == 0) or \
+            (positives_count[chem]['good'] == 0 and positives_count[chem]['bad'] > 0)]
+
+    tp = len(tp)
+    fp = len(fp)
+    fn = len(fn)
+    prec = tp / (tp + fp)
+    rec = tp / (tp + fn)
+    f1 = (2 * prec * rec) / (prec + rec)
+    prec, rec, f1
+    return tp, fp, fn, prec, rec, f1
+
+
+def get_key(chem):
+    # turn a chem into (mz, rt, intensity) for equal comparison
+    return (tuple(chem.isotopes), chem.rt, chem.max_intensity)
+
+
+def get_frag_events(controller, ms_level):
+    # get the fragmentation events for all chemicals for an ms level
+    filtered_frag_events = list(filter(lambda x: x.ms_level == ms_level, controller.mass_spec.fragmentation_events))
+    chem_to_frag_events = defaultdict(list)
+    for frag_event in filtered_frag_events:
+        key = get_key(frag_event.chem)
+        chem_to_frag_events[key].append(frag_event)
+    return dict(chem_to_frag_events)
+
+
+def get_absolute_intensity(chem, query_rt):
+    return chem.max_intensity * chem.chromatogram.get_relative_intensity(query_rt - chem.rt)
+
+
+def count_frag_events(key, chem_to_frag_events, min_ms1_intensity):
+    # count how many good and bad fragmentation events for each chemical (key)
+    frag_events = chem_to_frag_events[key]
+    good_count = 0
+    bad_count = 0
+    for frag_event in frag_events:
+        chem = frag_event.chem
+        query_rt = frag_event.query_rt
+        if get_absolute_intensity(chem, query_rt) < min_ms1_intensity:
+            bad_count += 1
+        else:
+            good_count += 1
+    return good_count, bad_count
+
+
+def get_chem_frag_counts(chem_list, chem_to_frag_events, min_ms1_intensity):
+    # get the count of good/bad fragmentation events for all chemicals in chem_list
+    results = {}
+    for i in range(len(chem_list)):
+        chem = chem_list[i]
+        key = get_key(chem)
+        try:
+            good_count, bad_count = count_frag_events(key, chem_to_frag_events, min_ms1_intensity)
+        except KeyError:
+            good_count = 0
+            bad_count = 0
+        results[chem] = {
+            'good': good_count,
+            'bad': bad_count
+        }
+    return results
